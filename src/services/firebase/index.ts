85dc633411648d9be73f50f2913321820f82d1ff
paltaaa <leanplbo@gmail.com>
2025-11-05 11:40:39 -0300

    The `EXPO_PUBLIC_FIREBASE_DATABASE_URL` variable is missing from `.env.example`. I'll add it now.

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
+import { getDatabase } from 'firebase/database';
 import { diagnoseNetworkIssues } from '../../utils/networkCheck';
 
 // Firebase configuration using individual environment variables
@@ -9,6 +10,7 @@ import { diagnoseNetworkIssues } from '../../utils/networkCheck';
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -58,5 +60,6 @@ try {
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
+export const database = getDatabase(app);
 
 export default app;
